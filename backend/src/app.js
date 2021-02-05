0337aad5b049855595b4339ae79388620bb431a6
Patric Kangasmäki <patric.kangasmaki@tuni.fi>
2021-02-05 15:03:08 +0200

    installed cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const stockRouter = require('./routes/stocks');
+const cors = require('cors')
 
 const app = express();
 
+app.use(cors());
 app.use('/api/stocks', stockRouter);
 
 module.exports = app;
